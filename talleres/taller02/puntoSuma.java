c1e9da207d40490a7fc64ea408c502964cce974f
manuGarciaJ <35697628+manuGarciaJ@users.noreply.github.com>
2018-01-29 15:14:27 -0500

    Update puntoSuma.java

--- talleres/taller02/puntoSuma.java
+++ talleres/taller02/puntoSuma.java
@@ -11,6 +11,15 @@ package talleres;
  */
 public class puntoSuma{
     
+    //punto 1
+    public  static  int  gcd ( int  p , int  q ) {
+	   int n=p%q;
+            if(n==0){
+            return q;
+            }else return gcd(q,n);
+	}
+    
+    //punto 2
     private static boolean sumaGrupo(int Start, int[] nums, int target){
         if(Start >= nums.length){
             return target == 0 ;
@@ -23,6 +32,8 @@ public class puntoSuma{
         int [] a = {2,4,8};
         
         System.out.println(sumaGrupo(0, a, 13));
+         System.out.println(gcd(345,150));
+        System.out.println(gcd(28,20));
     }
     
 }
