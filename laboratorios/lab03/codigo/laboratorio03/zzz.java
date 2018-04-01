0e1c512edef6291adc5f38d093d8d5ca8a20000b
manuGarciaJ <35697628+manuGarciaJ@users.noreply.github.com>
2018-04-01 13:36:12 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/laboratorio03/zzz.java
@@ -0,0 +1,51 @@
+
+/**
+ * Write a description of class zzz here.
+ *
+ * @author (Manuel García & Isaias Labrador)
+ * @version (abril 1, 2018)
+ */
+public class zzz
+{
+    public static String puntoDos(String param){
+     String aux= new String();
+     String salida= new String();
+     int next=0;
+     while(next<param.length()){
+     if(param.substring(next, next+1).equals("[")){
+       next++;  
+         while(next<param.length()&&param.substring(next, next+1).equals("[")==false&&param.substring(next, next+1).equals("]")==false){
+          aux= aux+param.substring(next, next+1);
+          next++;
+         }
+       salida= aux+salida;    
+       aux="";
+     }else if(param.substring(next, next+1).equals("]")){
+       next++;
+         while(next<param.length()&&param.substring(next, next+1).equals("[")==false&&param.substring(next, next+1).equals("]")==false){
+          aux= aux+param.substring(next, next+1);
+          next++;
+         }
+       salida= salida+aux;    
+       aux="";
+     }else{
+      while(next<param.length()&&param.substring(next, next+1).equals("[")==false&&param.substring(next, next+1).equals("]")==false){
+          aux= aux+param.substring(next, next+1);
+          next++;
+        }
+         salida= salida+aux; 
+         aux="";
+      }
+     }
+     return salida;
+    }
+    
+    public static void main(){
+      System.out.println(puntoDos("This_is_a_[Beiju]_text"));
+      System.out.println(puntoDos("[[]][][]Happy_Birthday_to_Tsinghua_University"));
+      System.out.println(puntoDos("asd[fgh[jkl"));
+      System.out.println(puntoDos("asd[fgh[jkl["));
+      System.out.println(puntoDos("[[a[[d[f[[g[g[h[h[dgd[fgsfa[f"));
+      System.out.println(puntoDos("asd[gfh[[dfh]hgh]fdfhd[dfg[d]g[d]dg"));
+    }
+}
