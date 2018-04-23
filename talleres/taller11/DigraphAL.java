67605cea3f4f65efc4ac8404fb40201f32165c93
ilabradors <33398937+ilabradors@users.noreply.github.com>
2018-04-23 18:47:53 -0500

    Add files via upload

--- talleres/taller11/DigraphAL.java
+++ talleres/taller11/DigraphAL.java
@@ -1,30 +1,60 @@
-import java.util.ArrayList;
-/**
- * Esta clase es una implementación de un digrafo usando listas de adyacencia
- * 
- * @author Mauricio Toro 
- * @version 1
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-public class DigraphAL extends Graph
-{
+package pruebas;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @see Grafo con listas
+ * Codigo esqueleto tomado de: https://github.com/mauriciotoro/ST0245-Eafit/blob/master/talleres/taller11/Java/Laboratorio1/DigraphAL.java
+ * y modificado por: Isaias Labrador Sánchez. Sin intencion de infringir los derechos de autor.
+ */
+public class DigraphAL{
+    
+    int tamaño = 0;
+    ArrayList [] nodos = new ArrayList [tamaño];
+    
+    public DigraphAL(int size)
+    {
+      this.tamaño = size;     
+    }
    
-   public DigraphAL(int size)
-   {
-     
-   }
-   
-   public void addArc(int source, int destination, int weight)
-   {
-     
-   }
-   
-   public int getWeight(int source, int destination)
-   {
-      
-   }
-  
-   public ArrayList<Integer> getSuccessors(int vertice)
-   {
-   
-   }
+    /**
+     * 
+     * @param source
+     * @param destination
+     * @param weight
+     * @see Añade peso a de un nodo a otro (distancia) 
+     */
+    public void addArc(int source, int destination, int weight)
+    {
+        this.nodos[source].add(destination, weight);
+    }
+
+    /**
+     * 
+     * @param source
+     * @param destination
+     * @return peso entre los dos vertices 
+     */
+    public int getWeight(int source, int destination)
+    {
+        return (int) this.nodos[source].get(destination);
+    }
+
+    /**
+     * 
+     * @param vertice
+     * @return vertices vecinos ArrayList<Integer> 
+     */
+    public ArrayList<Integer> getSuccessors(int vertice)
+    {
+        return this.nodos[vertice];
+    }
+    
 }
